fb1b3f4e7659f8ffa6c1b2a91a5a644d92aa6e2e
vishnugopy.suthaharan <vishnugopy.suthaharan@o4software.com>
2022-02-12 16:02:28 +0100

    home page

--- App.js
+++ App.js
@@ -1,20 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import Card from "./components/Card";
+import { TouchableOpacity, ScrollView } from "react-native-web";
+import { useState, useEffect } from "react";
 
 export default function App() {
+  const [HomeView, setHomeView] = useState(true);
+  const [taskItems, setTaskItems] = useState([]);
+
+  const [loaded] = useFonts({
+    RubikBold: require("./assets/fonts/Rubik-Bold.ttf"),
+    RubikLight: require("./assets/fonts/Rubik-Light.ttf"),
+  });
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity style={styles.headerButton}>
+          <Text
+            style={styles.headerText}
+            onClick={() => {
+              setHomeView(true);
+            }}
+          >
+            Home
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.headerButton}>
+          <Text
+            style={styles.headerText}
+            onClick={() => {
+              setHomeView(false);
+            }}
+          >
+            History
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      {HomeView === true ? (
+        <View style={styles.cardContainer}>
+          <Text>Home</Text>
+        </View>
+      ) : (
+        <ScrollView style={styles.historyItems}>
+          <Card date="11/02/2022" result="50" />
+          <Card date="11/02/2022" result="50" />
+        </ScrollView>
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    fontFamily: "RubikBlack",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+  },
+  headerText: {
+    fontFamily: "RubikLight",
+    fontSize: 24,
+    color: "#000",
+  },
+  historyItems: {
+    display: "flex",
+    flexDirection: "column",
+    flex: 1,
+    marginTop: 20,
   },
 });
---
+++ components/Card.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const Card = (props) => {
+  return (
+    <View style={styles.item}>
+      <Text style={styles.date}>{props.date}</Text>
+      <Text style={styles.result}>{props.result}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: "#FF316220",
+    width: "90%",
+    padding: 20,
+    borderRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginBottom: 10,
+    marginTop: 10,
+  },
+  date: {
+    fontFamily: "RubikLight",
+    color: "#000",
+  },
+  result: {
+    fontFamily: "RubikBold",
+    color: "#FF3162",
+    fontSize: 20,
+    fontWeight: "1000",
+  },
+});
+
+export default Card;
